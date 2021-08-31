806c619f80401d2839d8cc40743bb9e8309664f8
kpawlows <kp131320@ohio.edu>
2021-08-31 16:50:50 -0400

    Adding the Hello Git! program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Keith Pawlowski
+ *        Date: 8/31/2021
+ * Description: 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
